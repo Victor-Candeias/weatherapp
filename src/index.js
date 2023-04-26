8a5e4f80baccdbaa32168655dc5b506144081e02
Victor Candeias <victor.s.candeias@ctt.pt>
2023-04-26 15:46:27 +0100

    Update index.js
    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,4 +3,18 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// For deploy
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+/*
+    For debug 
+ReactDOM.render(
+    <App />, 
+    document.getElementById("root")
+);
+*/
